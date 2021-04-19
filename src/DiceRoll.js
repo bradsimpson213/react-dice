e2ec1cfccc397fb63f363e4eb0ae212f1e2bdf25
Brad Simpson <bradsimpson@icloud.com>
2021-04-19 16:12:01 -0400

    finished styling

--- src/DiceRoll.js
+++ src/DiceRoll.js
@@ -8,30 +8,55 @@ class DiceRoll extends Component {
     };
     constructor(props){
         super(props);
-        this.state = {die1: 'one', die2: "two", rollValue: 3};
+        this.state = { die1: 'one', die2: "two", rollValue: 3, rolling: false };
     };
 
     componentDidMount() {
         this.roll();
-    }
+    };
 
     roll = () => {
         const num1 = Math.floor(Math.random() * this.props.sides.length);
         const num2 = Math.floor(Math.random() * this.props.sides.length);
         const dieRoll1 = this.props.sides[num1];
         const dieRoll2 = this.props.sides[num2];
-        this.setState({die1: dieRoll1, die2: dieRoll2, rollValue: (num1 + num2 + 2) })
+        this.setState(
+            {
+                die1: dieRoll1, 
+                die2: dieRoll2, 
+                rollValue: (num1 + num2 + 2), 
+                rolling: true 
+            }
+        );
+        setTimeout(()=> {
+            this.setState({rolling: false})
+        }, 1000)
     };
  
     render(){
         return (
             <div className="dice-roll-container">
                 <div className="dice-container">
-                    <Die face={this.state.die1} />
-                    <Die face={this.state.die2} />
+                    <Die face={ this.state.die1 } 
+                        shake={ this.state.rolling } 
+                    />
+                    <Die face={ this.state.die2 }
+                        shake={ this.state.rolling }
+                    />
                 </div>
-                <div className="roll-text">You rolled a {this.state.rollValue}!</div>
-                <button className="roll-button" onClick={ this.roll }>Roll Dice!</button>
+                <div className="roll-text"
+                    style={ this.state.rolling ? { opacity: '0', transition: 'None' } : 
+                        { opacity: '1', transition: 'opacity 1.0s' }}
+                >
+                    You rolled a { this.state.rollValue }!
+                </div>
+                <button 
+                    className="roll-button" 
+                    onClick={ this.roll }
+                    disabled={ this.state.rolling }
+                >
+                    {this.state.rolling ? "Rolling..." : "Roll Dice!" }
+                </button>
             </div>
         );
     };
--- src/Die.js
+++ src/Die.js
@@ -5,7 +5,8 @@ class Die extends Component {
     render(){
         return (
             <div>
-                <i className={`Die fas fa-dice-${this.props.face}`} />
+                <i className={`Die fas fa-dice-${ this.props.face } 
+                    ${ this.props.shake ? 'shake-dice' : '' }`} />
             </div>
         );
     }
